f2a266b257814d5dc086213bb7612616dfbab8ea
Matias <matisilva18@hotmail.com>
2016-04-08 01:06:06 -0300

    Funcionalidad completa pero meteor sigue tirando errores funcionalidad solo en main.html

--- meteorServer/client/main.js
+++ meteorServer/client/main.js
@@ -2,4 +2,9 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
-import '../imports/ui/body.js';
+
+Template.yourLayout.created = function() {
+  $('head').append('<script type="text/javascript" src="lib/eventemitter2.min.js">');
+  $('head').append('<script type="text/javascript" src="lib/roslib.min.js">');
+  $('head').append('<script type="text/javascript" src="turtleChallenge.js">');
+}
